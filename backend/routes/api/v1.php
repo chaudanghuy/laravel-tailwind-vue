21dc4ec66420e12553543f3e85bb003df8e5de00
Châu Đăng Huy <chaudanghuy@gmail.com>
2023-09-04 07:40:25 +0700

    Add todolist

--- backend/routes/api/v1.php
+++ backend/routes/api/v1.php
@@ -4,7 +4,8 @@ use App\Http\Controllers\Api\V1\CompleTaskController;
 use App\Http\Controllers\Api\V1\TaskController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth:sanctum')->prefix('v1')->group(function() {
-  Route::apiResource('/tasks', TaskController::class);  
+Route::apiResource('/v1/tasks', TaskController::class);
+
+Route::middleware('auth:sanctum')->prefix('v1')->group(function() {    
   Route::patch('/tasks/{task}/complete', CompleTaskController::class);
 });
---
+++ todoapp-vue/src/components/tasks/Task.vue
@@ -0,0 +1,49 @@
+<template>
+<li class="list-group-item py-3">
+  <div class="d-flex justify-content-start align-items-center">
+    <input class="form-check-input mt-0" :class="completedClass" type="checkbox" :checked="task.is_completed" />
+    <div 
+      class="ms-2 flex-grow-1" :class="completedClass" 
+      title="Double click the text to edit or remove"
+      @dblclick="$event => isEdit = true"
+    >      
+      <div class="relative" v-if="isEdit">
+       <input type="text" 
+          class="editable-task" 
+          @keyup.esc="$event => isEdit = false"
+          v-focus
+          @keyup.enter="updateTask"
+        /> 
+      </div>
+      <span v-else>{{ task.name }}</span>
+    </div>
+    <!-- <div class="task-date">24 Feb 12:00</div> -->
+  </div>
+  <TaskActions @edit="$event => isEdit = true" v-show="!isEdit"/>
+</li>
+</template>
+
+<script setup>
+import {computed, ref} from "vue";
+import TaskActions from "./TaskActions.vue";
+import { updateTask } from "../../http/task-api";
+
+const props = defineProps({
+  task: Object
+})
+
+const emit = defineEmits(['updated'])
+
+const isEdit = ref(false)
+
+const completedClass = computed(() => props.task.is_completed ? "completed" : "")
+const vFocus = {
+  mounted: (el) => el.focus()
+}
+
+const updateTask = event => {
+  const updatedTask = { ...props.task, name: event.target.value}
+  isEdit.value = false
+  emit('updated', updatedTask)
+}
+</script>
