c0e85611787f440c9fe1838c3ac2977c50c2f225
Huy Chau <huy.chau@besmartee.com>
2023-09-09 18:48:58 +0700

    Authenticate Vue routes

--- todoapp-vue/src/http/api.js
+++ todoapp-vue/src/http/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
+axios.defaults.withCredential = true;
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_BASE_URL
+  baseURL: import.meta.env.VITE_BASE_URL,
+  withCredentials: true
 })
 
 export default api
